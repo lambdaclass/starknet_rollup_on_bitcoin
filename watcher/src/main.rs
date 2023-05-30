f0e8127bfeb7540dff1cf38a80a1521c43e66856
Nacho <amigoigna@gmail.com>
2023-05-30 18:49:41 -0300

    bitcoin-watcher

--- watcher/src/main.rs
+++ watcher/src/main.rs
@@ -1,12 +1,18 @@
-use std::{collections::HashSet};
+use lib::{Transaction, TransactionType};
 use reqwest;
 use serde_json;
+use std::collections::HashSet;
 use std::time::Duration;
 use tokio;
 
+mod abci_client;
+
+const LOCAL_SEQUENCER_URL: &str = "http://127.0.0.1:26657";
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let address = "1BitcoinEaterAddressDontSendf59kuE"; 
+    // TODO: Change this for our vanity address
+    let address = "1BitcoinEaterAddressDontSendf59kuE";
     let url = format!("https://blockchain.info/rawaddr/{}", address);
 
     let mut burned_transactions: HashSet<String> = HashSet::new();
@@ -17,13 +23,29 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         if let Some(transactions) = v["txs"].as_array() {
             for tx in transactions {
-                let hash = tx["hash"].as_str().expect("Transaction does not contain hash").clone();
+                let hash = tx["hash"]
+                    .as_str()
+                    .expect("Transaction does not contain hash")
+                    .clone();
                 if !burned_transactions.contains(hash) {
                     println!("About to burn ERC-20 on Barknet: {}", hash);
                     // call barknet and get status
-                    // if successful {
+                    let starknet_address = "0x0".to_string(); // TODO: Get address and tx data from bitcoin metadata
+                    let tx = Transaction::with_type(TransactionType::Mint {
+                        address: starknet_address.clone(),
+                        amount: 100,
+                        token_tick: "ordi".to_string(),
+                    });
+                    
+                    let result = abci_client::broadcast(tx, LOCAL_SEQUENCER_URL).await;
+
+                    if let Ok(_) = result {
+                        println!(
+                            "Minted succesfully hash {} to Starknet address {}",
+                            hash, &starknet_address
+                        );
                         burned_transactions.insert(hash.to_string());
-                    // }
+                    }
                 }
             }
         }
@@ -31,4 +53,3 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         tokio::time::sleep(Duration::from_secs(60)).await;
     }
 }
-
